e726937f7647223507c352d712159c6fc5473738
nyabuto-v <nyabutovice@gmail.com>
2022-09-28 13:09:05 +0300

    pass data to the next activity

--- app/src/main/java/com/nyabuto/myrestaurants4/MainActivity.java
+++ app/src/main/java/com/nyabuto/myrestaurants4/MainActivity.java
@@ -2,24 +2,33 @@ package com.nyabuto.myrestaurants4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
             }
         });
     }
